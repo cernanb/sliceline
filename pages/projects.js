a56cebbe0be6b3415056a4222c9993e771fabc68
Cernan Bernardo <cernanb@gmail.com>
2020-05-23 17:20:27 -0700

    Fix attributes to remove warnings

--- pages/projects.js
+++ pages/projects.js
@@ -1,7 +1,7 @@
 export default function Projects() {
   return (
-    <div class="py-4">
-      <div class="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
+    <div className="py-4">
+      <div className="border-4 border-dashed border-gray-200 rounded-lg h-96"></div>
     </div>
   )
 }

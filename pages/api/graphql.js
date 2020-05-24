d7514e6fcb65bccdfdf310f29d4c83645e059495
Cernan Bernardo <cernanb@gmail.com>
2020-05-24 14:43:22 -0700

    Add mongo connection

--- pages/api/graphql.js
+++ pages/api/graphql.js
@@ -1,4 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server-micro'
+import connectDb from '../../lib/mongoose'
 
 const typeDefs = gql`
   type Query {
@@ -23,4 +24,6 @@ export const config = {
   api: { bodyParser: false }
 }
 
-export default apolloServer.createHandler({ path: '/api/graphql' })
+const server = apolloServer.createHandler({ path: '/api/graphql' })
+
+export default connectDb(server)

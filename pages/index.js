c5871d971132c29e0d057fb07ec645945a83d67e
Cernan Bernardo <cernanb@gmail.com>
2020-05-23 17:20:13 -0700

    test query

--- pages/index.js
+++ pages/index.js
@@ -1,162 +1,176 @@
-export default function Home() {
+import { withApollo } from '../lib/apollo'
+import { useQuery } from '@apollo/react-hooks'
+import gql from 'graphql-tag'
+
+const HELLO_QUERY = gql`
+  query helloQuery {
+    sayHello
+  }
+`
+
+function Home() {
+  const { data, loading, error } = useQuery(HELLO_QUERY)
+  if (loading) return <div />
   return (
     <div>
-      <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
-        <div class="absolute inset-0">
-          <div class="bg-white h-1/3 sm:h-2/3"></div>
+      <div className="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+        <div className="absolute inset-0">
+          <div className="bg-white h-1/3 sm:h-2/3"></div>
         </div>
-        <div class="relative max-w-7xl mx-auto">
-          <div class="text-center">
-            <h2 class="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
+        <div className="relative max-w-7xl mx-auto">
+          <div className="text-center">
+            <h2 className="text-3xl leading-9 tracking-tight font-extrabold text-gray-900 sm:text-4xl sm:leading-10">
               From the blog
             </h2>
-            <p class="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
+            <p className="mt-3 max-w-2xl mx-auto text-xl leading-7 text-gray-500 sm:mt-4">
               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsa libero labore natus atque, ducimus sed.
             </p>
           </div>
-          <div class="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
-            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div class="flex-shrink-0">
+          <div className="mt-12 grid gap-5 max-w-lg mx-auto lg:grid-cols-3 lg:max-w-none">
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
                 <img
-                  class="h-48 w-full object-cover"
+                  className="h-48 w-full object-cover"
                   src="https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80"
                   alt=""
                 />
               </div>
-              <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div class="flex-1">
-                  <p class="text-sm leading-5 font-medium text-indigo-600">
-                    <a href="#" class="hover:underline">
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm leading-5 font-medium text-indigo-600">
+                    <a href="#" className="hover:underline">
                       Blog
                     </a>
                   </p>
-                  <a href="#" class="block">
-                    <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">Boost your conversion rate</h3>
-                    <p class="mt-3 text-base leading-6 text-gray-500">
+                  <a href="#" className="block">
+                    <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">Boost your conversion rate</h3>
+                    <p className="mt-3 text-base leading-6 text-gray-500">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius,
                       ut atque fuga culpa, similique sequi cum eos quis dolorum.
                     </p>
                   </a>
                 </div>
-                <div class="mt-6 flex items-center">
-                  <div class="flex-shrink-0">
+                <div className="mt-6 flex items-center">
+                  <div className="flex-shrink-0">
                     <a href="#">
                       <img
-                        class="h-10 w-10 rounded-full"
+                        className="h-10 w-10 rounded-full"
                         src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
                     </a>
                   </div>
-                  <div class="ml-3">
-                    <p class="text-sm leading-5 font-medium text-gray-900">
-                      <a href="#" class="hover:underline">
+                  <div className="ml-3">
+                    <p className="text-sm leading-5 font-medium text-gray-900">
+                      <a href="#" className="hover:underline">
                         Roel Aufderhar
                       </a>
                     </p>
-                    <div class="flex text-sm leading-5 text-gray-500">
+                    <div className="flex text-sm leading-5 text-gray-500">
                       <time dateTime="2020-03-16">Mar 16, 2020</time>
-                      <span class="mx-1">&middot;</span>
+                      <span className="mx-1">&middot;</span>
                       <span>6 min read</span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div class="flex-shrink-0">
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
                 <img
-                  class="h-48 w-full object-cover"
+                  className="h-48 w-full object-cover"
                   src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
                   alt=""
                 />
               </div>
-              <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div class="flex-1">
-                  <p class="text-sm leading-5 font-medium text-indigo-600">
-                    <a href="#" class="hover:underline">
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm leading-5 font-medium text-indigo-600">
+                    <a href="#" className="hover:underline">
                       Video
                     </a>
                   </p>
-                  <a href="#" class="block">
-                    <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                  <a href="#" className="block">
+                    <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
                       How to use search engine optimization to drive sales
                     </h3>
-                    <p class="mt-3 text-base leading-6 text-gray-500">
+                    <p className="mt-3 text-base leading-6 text-gray-500">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat
                       doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo
                       laudantium.
                     </p>
                   </a>
                 </div>
-                <div class="mt-6 flex items-center">
-                  <div class="flex-shrink-0">
+                <div className="mt-6 flex items-center">
+                  <div className="flex-shrink-0">
                     <a href="#">
                       <img
-                        class="h-10 w-10 rounded-full"
+                        className="h-10 w-10 rounded-full"
                         src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
                     </a>
                   </div>
-                  <div class="ml-3">
-                    <p class="text-sm leading-5 font-medium text-gray-900">
-                      <a href="#" class="hover:underline">
+                  <div className="ml-3">
+                    <p className="text-sm leading-5 font-medium text-gray-900">
+                      <a href="#" className="hover:underline">
                         Brenna Goyette
                       </a>
                     </p>
-                    <div class="flex text-sm leading-5 text-gray-500">
-                      <time datetime="2020-03-16">Mar 16, 2020</time>
-                      <span class="mx-1">&middot;</span>
+                    <div className="flex text-sm leading-5 text-gray-500">
+                      <time dateTime="2020-03-16">Mar 16, 2020</time>
+                      <span className="mx-1">&middot;</span>
                       <span>6 min read</span>
                     </div>
                   </div>
                 </div>
               </div>
             </div>
-            <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-              <div class="flex-shrink-0">
+            <div className="flex flex-col rounded-lg shadow-lg overflow-hidden">
+              <div className="flex-shrink-0">
                 <img
-                  class="h-48 w-full object-cover"
+                  className="h-48 w-full object-cover"
                   src="https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
                   alt=""
                 />
               </div>
-              <div class="flex-1 bg-white p-6 flex flex-col justify-between">
-                <div class="flex-1">
-                  <p class="text-sm leading-5 font-medium text-indigo-600">
-                    <a href="#" class="hover:underline">
+              <div className="flex-1 bg-white p-6 flex flex-col justify-between">
+                <div className="flex-1">
+                  <p className="text-sm leading-5 font-medium text-indigo-600">
+                    <a href="#" className="hover:underline">
                       {' '}
                       Case Study
                     </a>
                   </p>
-                  <a href="#" class="block">
-                    <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900">Improve your customer experience</h3>
-                    <p class="mt-3 text-base leading-6 text-gray-500">
+                  <a href="#" className="block">
+                    <h3 className="mt-2 text-xl leading-7 font-semibold text-gray-900">
+                      Improve your customer experience
+                    </h3>
+                    <p className="mt-3 text-base leading-6 text-gray-500">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo
                       recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.
                     </p>
                   </a>
                 </div>
-                <div class="mt-6 flex items-center">
-                  <div class="flex-shrink-0">
+                <div className="mt-6 flex items-center">
+                  <div className="flex-shrink-0">
                     <a href="#">
                       <img
-                        class="h-10 w-10 rounded-full"
+                        className="h-10 w-10 rounded-full"
                         src="https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                         alt=""
                       />
                     </a>
                   </div>
-                  <div class="ml-3">
-                    <p class="text-sm leading-5 font-medium text-gray-900">
-                      <a href="#" class="hover:underline">
+                  <div className="ml-3">
+                    <p className="text-sm leading-5 font-medium text-gray-900">
+                      <a href="#" className="hover:underline">
                         Daniela Metz
                       </a>
                     </p>
-                    <div class="flex text-sm leading-5 text-gray-500">
-                      <time datetime="2020-03-16">Mar 16, 2020</time>
-                      <span class="mx-1">&middot;</span>
+                    <div className="flex text-sm leading-5 text-gray-500">
+                      <time dateTime="2020-03-16">Mar 16, 2020</time>
+                      <span className="mx-1">&middot;</span>
                       <span>6 min read</span>
                     </div>
                   </div>
@@ -169,3 +183,5 @@ export default function Home() {
     </div>
   )
 }
+
+export default withApollo({ ssr: true })(Home)

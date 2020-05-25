dd3f40f7b39771175a1d1e298d85e2bbbeb24c9c
Cernan Bernardo <cernanb@gmail.com>
2020-05-25 09:17:56 -0700

    add nprogress package

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import '../styles/index.css'
 import Head from 'next/head'
 import Link from 'next/link'
+import Router from 'next/router'
+import NProgress from 'nprogress' //nprogress module
+import 'nprogress/nprogress.css'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (

22321e36995346c8a6fab1ba9b7260e6fb5d134b
Cernan Bernardo <cernanb@gmail.com>
2020-05-25 09:26:39 -0700

    update content

--- components/Slice.js
+++ components/Slice.js
@@ -2,7 +2,11 @@ import Link from 'next/link'
 const Slice = ({ slice }) => {
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img className="h-48 w-full object-cover" src="https://via.placeholder.com/150" alt="Sunset in the mountains" />
+      <img
+        className="h-48 w-full object-cover"
+        src="https://images.unsplash.com/photo-1542282811-943ef1a977c3?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9"
+        alt="Sunset in the mountains"
+      />
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">
           <Link href={`slices/[id]`} as={`/slices/${slice._id}`}>

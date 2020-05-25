ff4f70eadcd8299c27cec34e0b3d1064c2b30376
Cernan Bernardo <cernanb@gmail.com>
2020-05-25 15:28:50 -0700

    update font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,7 @@
 const defaultTheme = require('tailwindcss/defaultTheme')
 module.exports = {
   theme: {
-    extend: {
-      fontFamily: {
-        sans: ['Inter', ...defaultTheme.fontFamily.sans]
-      }
-    }
+    extend: {}
   },
   plugins: [require('@tailwindcss/ui')]
 }

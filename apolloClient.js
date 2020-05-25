6663e9611ae6ebdf4544f87e6285c4c034a64a25
Cernan Bernardo <cernanb@gmail.com>
2020-05-25 14:12:19 -0700

    update api url for dev and prod

--- apolloClient.js
+++ apolloClient.js
@@ -3,13 +3,18 @@ import { InMemoryCache } from 'apollo-cache-inmemory'
 import { HttpLink } from 'apollo-link-http'
 import fetch from 'isomorphic-unfetch'
 
+console.log(process.env.NODE_ENV)
+
 export default function createApolloClient(initialState, ctx) {
   // The `ctx` (NextPageContext) will only be present on the server.
   // use it to extract auth headers (ctx.req) or similar.
   return new ApolloClient({
     ssrMode: Boolean(ctx),
     link: new HttpLink({
-      uri: 'http://localhost:3000/api/graphql', // Server URL (must be absolute)
+      uri:
+        process.env.NODE_ENV === 'development'
+          ? 'http://localhost:3000/api/graphql'
+          : 'https://sliceline.now.sh/api/graphql', // Server URL (must be absolute)
       credentials: 'same-origin', // Additional fetch() options like `credentials` or `headers`
       fetch
     }),
